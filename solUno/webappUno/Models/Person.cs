b5d0f45204bba6c4a0e6ef72336009bf57af9a45
joel <joelsestrada2009@gmail.com>
2020-02-15 10:31:39 -0400

    Mi primer modelo

---
+++ solUno/webappUno/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace webappUno.Models
+{
+    public class Person
+    {
+        [Key]
+        public int PersonID { get; set; }
+
+        [Required(ErrorMessage ="Your fucking name, please")]
+        [Display(Name = "Nombre Completo")]
+        [StringLength(100,ErrorMessage ="Acortalo a 100")]
+
+        public string Name { get; set; }
+        [Display(Name="Tu carnet")]
+        public string Document { get; set; }
+    }
+}
